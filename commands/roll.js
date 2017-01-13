46dcdd7975230639745cf990a6a0abf439264abc
Anna Lawley <alawley1@gmail.com>
2017-01-13 11:53:17 -0800

    small refactor to clean up main function

--- commands/roll.js
+++ commands/roll.js
@@ -1,6 +1,14 @@
 var util = require('../util');
 
-module.exports = function (param) {
+function add(a, b) {
+  return a + b;
+}
+
+function roll(dieSize) {
+  return (Math.floor(Math.random() * dieSize) + 1);
+}
+
+module.exports = function(param) {
   var	channel	= param.channel;
   if (param.args[0].includes('d')) {
     var rollInfo = param.args[0].split('d');
@@ -10,17 +18,11 @@ module.exports = function (param) {
       } else {
         var diceCount = parseInt(rollInfo[0]);
         var diceSize = parseInt(rollInfo[1]);
-        function roll(dieSize) {
-          return (Math.floor(Math.random() * dieSize) + 1);
-        }
         var diceResults = [];
         for (i = 0; i < diceCount; i++) {
           diceResults.push(roll(diceSize));
         }
         var message = [param.args[0]];
-        function add(a, b) {
-          return a + b;
-        }
         var total = 0;
         if (param.args[1]) {
           total = diceResults.reduce(add, 0) + parseInt(param.args[1]);
