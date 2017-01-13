6bfb96f59785621303156df6ba385bff7f4f4c35
Anna Lawley <alawley1@gmail.com>
2017-01-12 16:44:01 -0800

    display results of simple dice rolls

--- commands/roll.js
+++ commands/roll.js
@@ -1,6 +1,22 @@
 var util = require('../util');
 
 module.exports = function (param) {
-  var	channel		= param.channel
-    util.postMessage(channel, 'You rolled dice');
+  var	channel	= param.channel;
+  var rollInfo = param.args[0].split('d');
+  var diceCount = parseInt(rollInfo[0]);
+  var diceSize = parseInt(rollInfo[1]);
+  function roll(dieSize) {
+    return (Math.floor(Math.random() * dieSize) + 1);
+  }
+  var diceResults = [];
+  for (i = 0; i < diceCount; i++) {
+    diceResults.push(roll(diceSize));
+  }
+  var info = [];
+  function add(a, b) {
+    return a + b;
+  }
+  info.push(diceResults);
+  info.push(diceResults.reduce(add, 0))
+  util.postMessage(channel, info.join('\n\n'));
 };
