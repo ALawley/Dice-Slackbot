6737c849411fcccf6167d9ae8167275a716b85aa
Anna Lawley <alawley1@gmail.com>
2017-01-13 10:57:07 -0800

    add error handling for invalid dice formats

--- commands/roll.js
+++ commands/roll.js
@@ -2,27 +2,36 @@ var util = require('../util');
 
 module.exports = function (param) {
   var	channel	= param.channel;
-  var rollInfo = param.args[0].split('d');
-  var diceCount = parseInt(rollInfo[0]);
-  var diceSize = parseInt(rollInfo[1]);
-  function roll(dieSize) {
-    return (Math.floor(Math.random() * dieSize) + 1);
-  }
-  var diceResults = [];
-  for (i = 0; i < diceCount; i++) {
-    diceResults.push(roll(diceSize));
-  }
-  var info = [param.args[0]];
-  function add(a, b) {
-    return a + b;
-  }
-  var total = 0;
-  if (param.args[1]) {
-    total = diceResults.reduce(add, 0) + parseInt(param.args[1]);
+  if (param.args[0].includes('d')) {
+    var rollInfo = param.args[0].split('d');
+    if (parseInt(rollInfo[0]) && parseInt(rollInfo[1])) {
+      var diceCount = parseInt(rollInfo[0]);
+      var diceSize = parseInt(rollInfo[1]);
+      function roll(dieSize) {
+        return (Math.floor(Math.random() * dieSize) + 1);
+      }
+      var diceResults = [];
+      for (i = 0; i < diceCount; i++) {
+        diceResults.push(roll(diceSize));
+      }
+      var message = [param.args[0]];
+      function add(a, b) {
+        return a + b;
+      }
+      var total = 0;
+      if (param.args[1]) {
+        total = diceResults.reduce(add, 0) + parseInt(param.args[1]);
+      } else {
+        total = diceResults.reduce(add, 0);
+      }
+      message.push(diceResults);
+      message.push(total);
+      util.postMessage(channel, message.join('\n'));
+    } else {
+      util.postMessage (channel, 'Please enter a valid die roll.');
+    }
   } else {
-    total = diceResults.reduce(add, 0);
+    util.postMessage (channel, 'Please enter a valid die roll.');
   }
-  info.push(diceResults);
-  info.push(total);
-  util.postMessage(channel, info.join('\n\n'));
+
 };
